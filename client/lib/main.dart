850ea724e8add25e1671df4c9995783558d75989
Page0526 <phuongtrang2652004@gmail.com>
2025-02-20 01:42:29 +0700

    Update Fitting Room

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
-
-
+import 'package:flutter/services.dart';
+import 'package:client_1/pages/fitting_room.dart';
 
 void main() {
-  runApp(const MyApp());
-}
 
+  runApp(MaterialApp(
+    theme: ThemeData(
+      scaffoldBackgroundColor: Colors.white
+    ),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => FittingRoom(),
+    },
+  ));
+}
 
 
