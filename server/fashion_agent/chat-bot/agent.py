9caaab4f4c0119d1e3553c03e256a46fac4f3300
trungviet17 <theseventeengv@gmail.com>
2025-03-05 14:03:11 +0700

    feat : update agent

--- server/fashion_agent/chat-bot/agent.py
+++ server/fashion_agent/chat-bot/agent.py
@@ -3,8 +3,14 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
-from langchain.chains.conversation.base import ConversationChain
+from langchain.chains.conversation.base import ConversationChain 
+import sys
+import os
+import warnings
 
+warnings.filterwarnings("ignore")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from knowledge_db.vector_store import KnowledgeDB
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GOOGLE_APIKEY")
@@ -16,12 +22,8 @@ class FashionAgent:
 
     """
 
-    def __init__(self, template_path: str):
+    def __init__(self):
 
-    
-        template = open(template_path, 'r')
-        self.template = template.read()
-        template.close()
 
         self.model = ChatGoogleGenerativeAI(
             model = "gemini-2.0-flash", 
@@ -30,15 +32,110 @@ class FashionAgent:
         )
 
 
-        self.memory = ConversationBufferMemory(return_messages=True)
+        self.memory = ConversationBufferMemory(
+            return_messages=True, 
+            memory_key = "chat_history", 
+            input_key = "input",
+            output_key = "response"
+        )
 
+        self.knowledge_db = KnowledgeDB()
+
+        try : 
+            self.knowledge_db.loading_vector_store() 
+        except FileNotFoundError:
+            print("Style vector store not found, creating new one")
+
+        self.qa_template = """
+            You are a fashion expert assistant providing accurate information based on the retrieved context.
+            Some rules you must follow:
+                1. Focus on topics related to fashion
+                2. If users ask about topics unrelated to fashion, politely decline and guide them back to fashion topics
+                3. Provide specific and practical advice
+                4. When possible, provide illustrative examples
+                5. Use friendly and accessible language
+            
+            Context information is below:
+            ---------------------
+            {context}
+            ---------------------
+            
+            Given this context, please answer the question. If the answer is not found in the context, 
+            say that you don't have information about that specific topic but provide relevant fashion advice 
+            based on your general knowledge.
+            
+            Chat History:
+            {chat_history}
+            
+            Question: {question}
+            Answer:
+        """
+
+        self.regular_template = """
+            You are a friendly, professional, and experienced fashion expert.
+            Your task is to provide information, advice on fashion trends, outfit coordination,
+            and related fashion knowledge.
+
+            Some rules you must follow:
+            1. Focus on topics related to fashion
+            2. If users ask about topics unrelated to fashion, politely decline and guide them back to fashion topics
+            3. Provide specific and practical advice
+            4. When possible, provide illustrative examples
+            5. Use friendly and accessible language
+
+            Conversation history:
+            {chat_history}
+
+            Latest question: {input}
+            Your response:
+        """
 
         self.conversation = ConversationChain(
             llm = self.model, 
             memory = self.memory, 
-            prompt = PromptTemplate(self.template), 
+            prompt = PromptTemplate(
+            template = self.regular_template,
+            input_variables=["chat_history", "input"]
+            ), 
             verbose = True
         )
+
+
+
+    def get_response(self, query: str): 
+        try: 
+            store_dir, _ = self.knowledge_db.routing(query)
+            if store_dir is None: 
+                response = self.conversation({"input": query})
+                return response['response']
+            
+            try: 
+                documents = self.knowledge_db.retrieval(query, store_dir)
+                if not documents or len(documents) == 0: 
+                    esponse = self.conversation({"input": query})
+                    return response['response']
+                
+                context = "\n".join([f"{i+1}. {doc.page_content}" for i, doc in enumerate(documents)])
+
+                prompt = self.qa_template.format(
+                    context = context,
+                    chat_history = self.memory.load_memory_variables({})["chat_history"],
+                    question = query
+                )
+                print(prompt)
+                response = self.model.invoke(prompt)
+
+                print(response)
+                
+                self.memory.save_context({"input": query}, {"response": response.content})
+                return response.content
+            
+            except Exception as e: 
+                print(e)
+                return "Error in retrieval"
+        except Exception as e:
+            print(e)
+            return "Error in routing (get response function)"
 
 
     def chat_simulator(self): 
@@ -52,14 +149,14 @@ class FashionAgent:
             if user_input == "exit": 
                 break
 
-            response = self.conversation(user_input)
+            response = self.get_response(user_input)
             print(f"Bot: {response}")
 
 
     
 
 if __name__ == "__main__": 
-    agent = FashionAgent("templates/fashion_template.txt")
+    agent = FashionAgent()
     agent.chat_simulator()
 
 
